e9a16ed27837603a6fe2f900143c16fc05d33ada
lovcFDS <1175020474@qq.com>
2020-05-07 11:50:48 +0800

    create

---
+++ clock/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.刷新天气ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.输入城市ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.刷新频率ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox2 = new System.Windows.Forms.ToolStripTextBox();
+            this.设置颜色ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox3 = new System.Windows.Forms.ToolStripTextBox();
+            this.bing每日壁纸ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.开启炫彩ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.锁定ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label4 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Mistral", 38.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Azure;
+            this.label1.Location = new System.Drawing.Point(53, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 61);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "时间";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ForeColor = System.Drawing.Color.Azure;
+            this.label3.Location = new System.Drawing.Point(59, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "日期";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("楷体", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.Azure;
+            this.label2.Location = new System.Drawing.Point(58, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "天气";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.刷新天气ToolStripMenuItem,
+            this.输入城市ToolStripMenuItem,
+            this.刷新频率ToolStripMenuItem,
+            this.设置颜色ToolStripMenuItem,
+            this.bing每日壁纸ToolStripMenuItem,
+            this.开启炫彩ToolStripMenuItem,
+            this.锁定ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(151, 180);
+            // 
+            // 刷新天气ToolStripMenuItem
+            // 
+            this.刷新天气ToolStripMenuItem.Name = "刷新天气ToolStripMenuItem";
+            this.刷新天气ToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.刷新天气ToolStripMenuItem.Text = "刷新天气";
+            this.刷新天气ToolStripMenuItem.Click += new System.EventHandler(this.刷新天气ToolStripMenuItem_Click);
+            // 
+            // 输入城市ToolStripMenuItem
+            // 
+            this.输入城市ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1});
+            this.输入城市ToolStripMenuItem.Name = "输入城市ToolStripMenuItem";
+            this.输入城市ToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.输入城市ToolStripMenuItem.Text = "输入城市";
+            this.输入城市ToolStripMenuItem.Click += new System.EventHandler(this.输入城市ToolStripMenuItem_Click);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+            this.toolStripTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.toolStripTextBox1_KeyDown);
+            // 
+            // 刷新频率ToolStripMenuItem
+            // 
+            this.刷新频率ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox2});
+            this.刷新频率ToolStripMenuItem.Name = "刷新频率ToolStripMenuItem";
+            this.刷新频率ToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.刷新频率ToolStripMenuItem.Text = "刷新频率";
+            this.刷新频率ToolStripMenuItem.Click += new System.EventHandler(this.刷新频率ToolStripMenuItem_Click);
+            // 
+            // toolStripTextBox2
+            // 
+            this.toolStripTextBox2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.toolStripTextBox2.Name = "toolStripTextBox2";
+            this.toolStripTextBox2.Size = new System.Drawing.Size(100, 23);
+            this.toolStripTextBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.toolStripTextBox2_KeyDown);
+            this.toolStripTextBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.toolStripTextBox2_KeyPress);
+            // 
+            // 设置颜色ToolStripMenuItem
+            // 
+            this.设置颜色ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox3});
+            this.设置颜色ToolStripMenuItem.Name = "设置颜色ToolStripMenuItem";
+            this.设置颜色ToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.设置颜色ToolStripMenuItem.Text = "设置颜色";
+            this.设置颜色ToolStripMenuItem.Click += new System.EventHandler(this.设置颜色ToolStripMenuItem_Click);
+            // 
+            // toolStripTextBox3
+            // 
+            this.toolStripTextBox3.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.toolStripTextBox3.Name = "toolStripTextBox3";
+            this.toolStripTextBox3.Size = new System.Drawing.Size(100, 23);
+            this.toolStripTextBox3.Text = "RGB值以点分隔";
+            this.toolStripTextBox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.toolStripTextBox3_KeyDown);
+            this.toolStripTextBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.toolStripTextBox3_KeyPress);
+            // 
+            // bing每日壁纸ToolStripMenuItem
+            // 
+            this.bing每日壁纸ToolStripMenuItem.Name = "bing每日壁纸ToolStripMenuItem";
+            this.bing每日壁纸ToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.bing每日壁纸ToolStripMenuItem.Text = "bing每日壁纸";
+            this.bing每日壁纸ToolStripMenuItem.Click += new System.EventHandler(this.bing每日壁纸ToolStripMenuItem_Click);
+            // 
+            // 开启炫彩ToolStripMenuItem
+            // 
+            this.开启炫彩ToolStripMenuItem.Name = "开启炫彩ToolStripMenuItem";
+            this.开启炫彩ToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.开启炫彩ToolStripMenuItem.Text = "开启炫彩";
+            this.开启炫彩ToolStripMenuItem.Click += new System.EventHandler(this.开启炫彩ToolStripMenuItem_Click);
+            // 
+            // 锁定ToolStripMenuItem
+            // 
+            this.锁定ToolStripMenuItem.Name = "锁定ToolStripMenuItem";
+            this.锁定ToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.锁定ToolStripMenuItem.Text = "锁定";
+            this.锁定ToolStripMenuItem.Click += new System.EventHandler(this.锁定ToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Pristina", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Azure;
+            this.label4.Location = new System.Drawing.Point(261, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 29);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "城市";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(442, 482);
+            this.ControlBox = false;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(950, 0);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "h";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 刷新天气ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 输入城市ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 锁定ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 开启炫彩ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 刷新频率ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox2;
+        private System.Windows.Forms.ToolStripMenuItem 设置颜色ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox3;
+        private System.Windows.Forms.ToolStripMenuItem bing每日壁纸ToolStripMenuItem;
+    }
+}
+
---
+++ clock/Form1.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using 壁纸;
+using System.Threading;
+
+namespace clock
+{
+    public partial class Form1 : Form
+    {
+        String ymd;
+        string city,cityBuffer;
+        string filename = "city.txt";
+        bool b = true;                  //用来保存位置锁定与否的变量
+        public Form1()
+        {
+            InitializeComponent();
+            ReadFile();
+            getWeather(city);
+            
+        }
+
+        //初始化时获取用户上次保存的城市名
+
+        private void ReadFile()
+        {
+            FileStream file = new FileStream(filename,FileMode.OpenOrCreate,FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(file,Encoding.Default);
+            StreamWriter sw = new StreamWriter(file,Encoding.UTF8);
+            if (sr.ReadLine() == null)
+            {
+                city = "济南";
+                sw.Write(city);
+                sw.Close();
+            }
+            else
+            {
+                file.Close();
+                city = File.ReadAllText(filename);
+            }
+            
+        }
+        //随机数生成颜色
+        private void RandomColor()
+        {
+            int a, b, g;
+            Random r = new Random();
+            a = r.Next(0, 256);
+            b = r.Next(0, 256);
+            g = r.Next(0, 256);
+            label1.ForeColor = Color.FromArgb(a, b, g);
+            label2.ForeColor = Color.FromArgb(a, b, g);
+            label3.ForeColor = Color.FromArgb(a, b, g);
+            label4.ForeColor = Color.FromArgb(a, b, g);
+        }
+
+        //更改城市文件内的存储
+        private void WriteFile(string cityName)
+        {
+            FileStream file = new FileStream(filename, FileMode.Create);
+            StreamWriter sw = new StreamWriter(file,Encoding.UTF8);
+            if (cityName != null)
+            {
+                sw.Write(cityName);
+                sw.Close();
+            }
+            file.Close();
+        }
+
+        //时间刷新函数
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label3.Text = "";
+            label1.Text = DateTime.Now.ToLongTimeString();
+            String week = DateTime.Today.DayOfWeek.ToString();
+            if (week == "Monday")
+            {
+                label3.Text = "周一";
+            }else if (week == "Tuesday")
+            {
+                label3.Text = "周二";
+            }else if (week == "Wednesday")
+            {
+                label3.Text = "周三";
+            }else if(week=="Thursday")
+            {
+                label3.Text = "周四";
+            }else if (week == "Friday")
+            {
+                label3.Text = "周五";
+            }else if(week=="Saturday")
+            {
+                label3.Text = "周六";
+            }
+            else
+            {
+                label3.Text = "周日";
+            }
+            label3.Text += "  ";
+            ymd = DateTime.Now.Year.ToString() + "年" + DateTime.Now.Month.ToString() + "月" + DateTime.Now.Day.ToString() + "日";
+            label3.Text += ymd;
+            if (b1)
+            {
+                RandomColor();
+            }
+        }
+        int startx=0, starty=0;//保存当前鼠标位置
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Button == MouseButtons.Left)&& b)
+            {
+                this.Left += e.X - startx;
+                this.Top += e.Y - starty;
+            }
+        }
+
+      
+        //鼠标移动和鼠标右击菜单事件
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                startx = e.X;
+                startx = e.Y;
+            }
+            
+            if (e.Button == MouseButtons.Right)
+            {
+                Point p = new Point(MousePosition.X, MousePosition.Y);
+                contextMenuStrip1.Show(p);
+            }
+        }
+        //web引用 
+        cn.com.webxml.www.WeatherWebService w = new cn.com.webxml.www.WeatherWebService();
+        //刷新天气
+        private void 刷新天气ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            getWeather(city);
+
+        }
+        //更改城市的天气
+        private void toolStripTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) 
+            {
+                cityBuffer = city;
+                city = toolStripTextBox1.Text;
+                Thread t = new Thread(getWeatherThread);
+                t.Start();
+                WriteFile(city);
+            }
+        }
+        //开辟新线程  
+        private void getWeatherThread()
+        {
+            getWeather(city);
+        }
+        //清空按钮之后的文本框
+        private void 输入城市ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripTextBox1.Text = "";
+        }
+        //关闭主程序
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void 锁定ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!b)
+            {
+                b = true;
+                锁定ToolStripMenuItem.Text = "锁定";
+            }
+            else
+            {
+                b = false;
+                锁定ToolStripMenuItem.Text = "解除锁定";
+            }
+        }
+        //炫彩控制
+        bool b1 = false;
+        private void 开启炫彩ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!b1)
+            {
+                b1 = true;
+                开启炫彩ToolStripMenuItem.Text = "关闭炫彩";
+            }
+            else
+            {
+                b1 = false;
+                开启炫彩ToolStripMenuItem.Text = "开启炫彩";
+            }
+        }
+
+        private void 刷新频率ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripTextBox2.Text = "";
+        }
+
+        private void toolStripTextBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != 8 && !Char.IsDigit(e.KeyChar))             //只能输入数字
+            {
+                e.Handled = true;
+            }
+        }
+        //修改时间刷新频率
+        private void toolStripTextBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                timer1.Interval = Convert.ToInt32(toolStripTextBox2.Text);
+            }
+        }
+
+        private void 设置颜色ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripTextBox3.Text = "";
+        }
+
+        int[] colorArgbs=new int[3];
+        private void toolStripTextBox3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string[] colors = toolStripTextBox3.Text.Split('.');
+                try
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        colorArgbs[i] = Convert.ToInt32(colors[i]);
+                    }
+
+                    Color color = new Color();
+                    color = Color.FromArgb(colorArgbs[0], colorArgbs[1], colorArgbs[2]);
+                    setForeColor(color);
+                }
+                catch
+                {
+                    MessageBox.Show("请按照格式输入：\n255.255.255");
+                }
+            }
+        }
+        //更改颜色
+        private void setForeColor(Color color)
+        {
+            label1.ForeColor = color;
+            label2.ForeColor = color;
+            label3.ForeColor = color;
+            label4.ForeColor = color;
+        }
+
+        private void toolStripTextBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != 8 && !Char.IsDigit(e.KeyChar) && e.KeyChar != 46)              //如果不是输入数字就不让输入
+            {
+                e.Handled = true;
+            }
+        }
+        
+        private void bing每日壁纸ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(getWallPaper);
+            t.Start();
+        }
+
+        void getWallPaper()
+        {
+            bingWallpaper bw = new bingWallpaper();
+            bw.setWallpaper();
+        }
+        //调用web获取天气情况
+        void getWeather(string city)
+        {
+            label2.Text = "\n";
+            try
+            {
+                String[] weather = w.getWeatherbyCityName(city);
+                if (weather[0].CompareTo("查询结果为空！") == 0)
+                {
+                    label2.Text = weather[0];
+                    city = cityBuffer;
+                }
+                else
+                {
+                    //将天气情况写入文件
+                    string path = "weather.txt";
+                    FileStream fileStream = new FileStream(path, FileMode.OpenOrCreate);
+                    StreamWriter sw = new StreamWriter(fileStream);
+                    for (int j = 0; j < weather.Length; j++)
+                    {
+                        if (j == 2 || j == 3 || j == 4 || j == 8 || j == 9 || j == 15 || j == 16 || j == 20 || j == 21) ;
+                        else
+                        {
+                            sw.WriteLine(weather[j]);
+                        }
+                    }
+                    sw.Close();
+                    //并对返回的字符串组进行修改删除
+                    String str = ((weather[10].Replace("；", "\n")).Replace("。", "\n")).Remove(0, 7);
+                    label4.Text = weather[0] + "\n" + weather[1];
+                    for (int i = 0; i < 10; i++)
+                    {
+
+                        if (i == 0 || i == 1 || i == 2 || i == 3 || i == 4 || i == 8 || i == 9) ;
+                        else if (i == 5)
+                        {
+                            label2.Text += weather[i] + "\t";
+                        }
+                        else if (i == 6)
+                        {
+                            label2.Text += weather[6].Remove(0, 5) + "\r\n";
+                        }
+                        else
+                        {
+                            label2.Text += weather[i] + "\r\n";
+                        }
+                    }
+                    label2.Text += str;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("请联网后重试！");
+            }
+        }
+        
+    }
+}
